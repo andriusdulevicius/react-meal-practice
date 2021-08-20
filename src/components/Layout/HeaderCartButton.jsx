65baea85dd3ecdc251282905d6f979f1af1140e6
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-20 09:27:36 +0300

    use reducer start

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,19 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.length;
   return (
     <button onClick={props.showModal} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,7 +1,33 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  // state = cartState
+  return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  const addItemToCartHandler = (item) => {
+    console.log('adding item');
+  };
+  const removeItemFromCartHandler = (id) => {
+    console.log('removing item');
+  };
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
+  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
 };
 
 export default CartProvider;
