f9685f875598fe7e4c1c1ab62497b7d0e2dfb5a3
AndriusDulevicius <andriusdulevicius@email.com>
2021-08-23 08:21:49 +0300

    cart item style and structure changed

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -4,9 +4,15 @@ import classes from './CartItem.module.css';
 const CartItem = ({ item, cartCtx }) => {
   return (
     <li key={item.id} className={classes['cart-item']}>
-      <span>{item.name}</span>
-      <button>Qty: {item.amount}</button>
-      <div className='inc-dec'>
+      <div>
+        <h2>{item.name}</h2>
+        <div className={classes.summary}>
+          <span className={classes.price}>{item.price}</span>
+          <span className={classes.amount}>Qty: {item.amount}</span>
+        </div>
+      </div>
+
+      <div className={classes.actions}>
         <button onClick={() => cartCtx.removeItem(item.id)}>-</button>
         <button onClick={() => cartCtx.addItem(item)}>+</button>
       </div>
